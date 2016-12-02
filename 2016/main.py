cea1506a9562991228135ce820d094a8d44eb2be
Cameron Bird <cambird3@gmail.com>
2016-12-02 15:24:49 +0000

    Day 2

--- 2016/main.py
+++ 2016/main.py
@@ -59,4 +59,68 @@ def day1():
 
     print('Bunny HQ is ' + str(abs(part2loc[0]) + abs(part2loc[1])) + ' blocks away')
 
+def day2():
+    inp = []
+    with open('day2.txt','r') as f:
+        for line in f:
+            inp.append(list(line))
+
+    x = 1
+    y = 1
+    keypad = [['1', '2', '3'],
+              ['4', '5', '6'],
+              ['7', '8', '9']]
+    code = ''
+
+    for instructs in inp:
+        for i in instructs:
+            if i == 'U':
+                y -= 1
+            elif i == 'D':
+                y += 1
+            elif i == 'L':
+                x -= 1
+            elif i == 'R':
+                x += 1
+
+            x = max(0, min(2, x))
+            y = max(0, min(2, y))
+
+        code += keypad[y][x]
+
+    print('Bathroom code: ' + code)
+
+    x = 0
+    y = 2
+    keypad = [[None, None, '1', None, None],
+              [None, '2',  '3', '4',  None],
+              ['5',  '6',  '7', '8',  '9' ],
+              [None, 'A',  'B', 'C',  None],
+              [None, None, 'D', None, None]]
+    code = ''
+
+    for instructs in inp:
+        for i in instructs:
+            if i == 'U':
+                if y > 0:
+                    if keypad[y-1][x] != None:
+                        y -= 1
+            elif i == 'D':
+                if y < 4:
+                    if keypad[y+1][x] != None:
+                        y += 1
+            elif i == 'L':
+                if x > 0:
+                    if keypad[y][x-1] != None:
+                        x -= 1
+            elif i == 'R':
+                if x < 4:
+                    if keypad[y][x+1] != None:
+                        x += 1
+
+        code += keypad[y][x]
+
+    print('Bathroom code: ' + code)
+
 day1()
+day2()
